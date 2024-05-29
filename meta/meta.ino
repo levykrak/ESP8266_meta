b39b6a8d162f1bd9b20559809a9c177ad5096599
Klimczyk Arkadiusz <aklimczyk@inpost.pl>
2024-05-29 12:51:25 +0200

    zmiana obslugi GPS

--- meta/meta.ino
+++ meta/meta.ino
@@ -1,14 +1,14 @@
 #include <espnow.h>
 #include <ESP8266WiFi.h>
 #include <TimeLib.h>    // http://www.pjrc.com/teensy/td_libs_Time.html
-#include <TinyGPS++.h>  // http://arduiniana.org/libraries/tinygpsplus/
+#include <TinyGPS.h>  // http://arduiniana.org/libraries/tinygpsplus/
 #include <SPI.h>
 #include <Adafruit_GFX.h>      // http://github.com/adafruit/Adafruit-GFX-Library
 #include <Adafruit_SSD1306.h>  // http://github.com/adafruit/Adafruit_SSD1306
 #include <SoftwareSerial.h>
 // #include <WiFi.h>
 
-//#define Debug
+#define Debug
 // REPLACE WITH THE MAC Address of your receiver
 uint8_t broadcastAddress[] = { 0xE8, 0x9F, 0x6D, 0x93, 0xF6, 0x50 };
 
@@ -33,23 +33,27 @@ bool wyscig_prawa = 0;
 #define SCREEN_ADDRESS 0x3C  ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-word licznik;
+// word licznik;
 bool delivery = 0;
 unsigned long lastButtonchange = 0, syncMillis = 0, pushTime = 0, lewa_czas = 0, lewa_czas_wyswietlacz = 0, prawa_czas = 0;
 word lewa_licznik = 0, prawa_licznik = 0, lewa_licznik_bufor = 0, prawa_licznik_bufor = 0, prawa_licznik_filter = 0, lewa_licznik_filter = filter;
 unsigned char LCDrefreshSecond = 0;
-static const int RXPin = 13, TXPin = 13;
 static const uint32_t GPSBaud = 9600;
 unsigned long cutnumber();
+long ping;
 //String timeString();
 // String millisToTimeString();
 //bool wyscig = 1, ready = 0;
 const long interval = 5000;
 long previousMillis;
 
-TinyGPSPlus gps;
-tmElements_t tm;
-SoftwareSerial ss(RXPin, TXPin);
+static const int RXPin = 13, TXPin = 13;   
+TinyGPS gps;
+// tmElements_t tm;
+SoftwareSerial SerialGPS(RXPin, TXPin);
+// Offset hours from gps time (UTC)
+const int offset = 2;   // Central European Time
+time_t prevDisplay = 0; // when the digital clock was displayed
 
 // Structure example to receive data
 // Must match the sender structure
@@ -77,6 +81,7 @@ void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
     Serial.println("Delivery fail");
 #endif
     delivery = 0;
+    ping++;
   }
 }
 //---------------------------------------------
@@ -93,14 +98,13 @@ void OnDataRecv(uint8_t *mac, uint8_t *incomingData, uint8_t len) {
     lewa_licznik_filter = filter;  //zerujemy filtry
     prawa_licznik_filter = filter;
   }
-
   esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));  //wysylamy status GPS
 }
 
 
 void setup() {
   Serial.begin(115200);
-  ss.begin(GPSBaud);
+  SerialGPS.begin(GPSBaud);
   pinMode(PPSPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(PPSPin), PPS, RISING);
 
@@ -146,20 +150,33 @@ void setup() {
 void loop() {
 
   //obsluga GPS uart
-  while (ss.available() > 0) {
-    gps.encode(ss.read());
-    if (tm.Hour != 0) {
-      ready = 1;
-      myData.status = ready;
+   while (SerialGPS.available()) {
+    if (gps.encode(SerialGPS.read())) { // process gps messages
+      // when TinyGPS reports new data...
+      unsigned long age;
+      int Year;
+      byte Month, Day, Hour, Minute, Second;
+      gps.crack_datetime(&Year, &Month, &Day, &Hour, &Minute, &Second, NULL, &age);
+      if (age < 500) {
+        // set the Time to the latest GPS reading
+        setTime(Hour, Minute, Second, Day, Month, Year);
+        adjustTime(offset * SECS_PER_HOUR);
+        
+      }
+    }
+  }
+  if (timeStatus()!= timeNotSet) {
+    if (now() != prevDisplay) { //update the display only if the time has changed
+      prevDisplay = now();
+      //digitalClockDisplay();  
     }
   }
 
   //sprawdzamy czy wiecej niz sekunda od PPS
   if (millis() - syncMillis >= 1100) {
     ready = 0;
-    myData.status = ready;
   }
-
+  myData.status = ready;
 
 
   // if (millis() - previousMillis >= interval && wyscig == 1) {
@@ -183,14 +200,31 @@ void loop() {
     display.setTextSize(2);
     display.setTextColor(WHITE);
     display.setCursor(0, 0);
-    display.println(lewa_licznik_bufor);
+    // display.println(lewa_licznik_bufor);   
+    // display.println(esp_now_get_peer_rssi);
     display.print("GPS ");
     if (ready) {
       display.println("OK");
     } else {
       display.println("X");
     }
+
+    if (delivery) {
+      display.print("ping "); display.println(ping);
+      }
+    else {
+      display.print("ping "); display.println(ping);
+      }
+  
     display.println(prawa_licznik_bufor);
+    display.println(second());
+
+    // if (millis() % 1000 >= syncMillis % 1000) {
+    //     display.println((tm.Hour * 3600 + tm.Minute * 60 + tm.Second) * 1000 + millis() % 1000 - syncMillis % 1000);
+    //   } else {
+    //     display.println((tm.Hour * 3600 + tm.Minute * 60 + tm.Second) * 1000 + 1000 + millis() % 1000 - syncMillis % 1000);
+    //   }
+    
     display.display();
   }
 
@@ -207,9 +241,10 @@ void loop() {
   }
   */
 
-  // wysylamy co 5 sekund info o statusie i czasie toru
+  // ############################ wysylamy co 5 sekund info o statusie i czasie toru ####################
   unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= 5000) {
+  // if (currentMillis - previousMillis >= 5000) {
+    if (currentMillis - previousMillis >= 200) {
     // save the last time you updated the DHT values
     previousMillis = currentMillis;
     myData.lewa_czas = lewa_czas;
@@ -226,16 +261,14 @@ void loop() {
 
 ICACHE_RAM_ATTR void PPS() {  //przerwanie od PPS
   syncMillis = millis();
-  licznik++;
+    ready = 1;   //jesli nie ma przerwania to sie zeruje
+    
 
-  if (gps.time.isUpdated() && gps.date.year() > 2015 && !timeStatus()) {
-
-    setTime(gps.time.hour(), gps.time.minute(), gps.time.second(), gps.date.day(), gps.date.month(), gps.date.year());
-    //adjustTime(UTCcorrection + 1);
-    //detachInterrupt(digitalPinToInterrupt(PPSPin));
-    //syncUNIXtime = now();
-  } else
-    breakTime(now(), tm);
+    Serial.print(hour());
+    Serial.print(":");
+    Serial.print(minute());
+    Serial.print(":");
+    Serial.println(second());
 }
 
 ICACHE_RAM_ATTR void lewa() {
@@ -245,9 +278,9 @@ ICACHE_RAM_ATTR void lewa() {
       lewa_licznik_filter = filter;
       wyscig_lewa = 0;
       if (millis() % 1000 >= syncMillis % 1000) {
-        lewa_czas = (tm.Hour * 3600 + tm.Minute * 60 + tm.Second) * 1000 + millis() % 1000 - syncMillis % 1000;
+        lewa_czas = (hour() * 3600 + minute() * 60 + second()) * 1000 + millis() % 1000 - syncMillis % 1000;
       } else {
-        lewa_czas = (tm.Hour * 3600 + tm.Minute * 60 + tm.Second) * 1000 + 1000 + millis() % 1000 - syncMillis % 1000;
+        lewa_czas = (hour() * 3600 + minute() * 60 + second()) * 1000 + 1000 + millis() % 1000 - syncMillis % 1000;
       }
     }
   }
@@ -261,9 +294,9 @@ ICACHE_RAM_ATTR void prawa() {
       prawa_licznik_filter = filter;
       wyscig_prawa = 0;
       if (millis() % 1000 >= syncMillis % 1000) {
-        prawa_czas = (tm.Hour * 3600 + tm.Minute * 60 + tm.Second) * 1000 + millis() % 1000 - syncMillis % 1000;
+        prawa_czas = (hour() * 3600 + minute() * 60 + second()) * 1000 + millis() % 1000 - syncMillis % 1000;
       } else {
-        prawa_czas = (tm.Hour * 3600 + tm.Minute * 60 + tm.Second) * 1000 + 1000 + millis() % 1000 - syncMillis % 1000;
+        prawa_czas = (hour() * 3600 + minute() * 60 + second()) * 1000 + 1000 + millis() % 1000 - syncMillis % 1000;
       }
     }
   }
